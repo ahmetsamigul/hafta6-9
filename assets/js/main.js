a2ad9ce0b568826f241af2be202449c51a71737f
Ahmet Sami Gül <a_sami_gul@Ahmet-MacBook-Air-4.local>
2025-02-09 19:12:18 +0300

    team members added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,2 @@
-console.log(window.location.pathname);
 const activeMenuItem = document.getElementById(window.location.pathname.replace("/","").split(".")[0]);
-activeMenuItem.style.color = "gold";
+activeMenuItem.style.color = "goldenrod";
